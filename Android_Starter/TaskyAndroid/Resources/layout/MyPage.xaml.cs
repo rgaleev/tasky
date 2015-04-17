99cff11c0c006f615a3cd944e5af3c032ea0ea21
Roman Galeev <romanrgaleev@gmail.com>
2015-04-18 00:06:01 +0300

    initial commit

---
+++ Android_Starter/TaskyAndroid/Resources/layout/MyPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TaskyAndroid
+{
+	public partial class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
